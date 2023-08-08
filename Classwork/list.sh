162496f5d36cf701b04cc12f03ae85cd226722d1
prashanthkb <126637555+Prashanthkb28@users.noreply.github.com>
2023-08-09 02:09:42 +0530

    Create list.sh

---
+++ Classwork/list.sh
@@ -0,0 +1,8 @@
+#to list the content
+ls         #list the dir content in present working dir
+ls *       #list all the dir
+ls -lh     #human readable  form
+ls -a      # list hidden files
+ls -r      #lisz in reverse order 
+ls *.jpg    #list all jpg files
+ls ~       #list the dir contentd of  users home directory 
